9437e2a6bcddb87c364ed9507088f3f4106bbe60
Gonzalo López <gonzalo.lopez@blinklearning.com>
2022-05-10 12:58:13 +0200

    Subida inicial proyecto React

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        ¡Lo conseguiste!
+      </header>
+      <div>
+        <p>
+          Y como lo prometido es deuda... te voy a revelar mi secreto.
+        </p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
